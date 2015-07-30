1796f808b8d75cda287528326e1cec254fdf15be
Manfred Klaffenboeck <fred_klaf@gmx.net>
2015-07-30 18:55:31 +0200

    config changes for rserve

--- config/initializers/setup_r.rb
+++ config/initializers/setup_r.rb
@@ -2,6 +2,8 @@ require "rinruby"
 require "rserve"
 
 R.eval "library(mlegp); library(lhs);"
+R.eval("library('Rserve');")
+R.eval("Rserve(args='--vanilla');")
 
 RBridge = Rserve::Connection.new
 RBridge.eval("library(mlegp);")
