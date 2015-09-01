432990711049185a5c8b9d9bfed638ce9a41b9ea
Manfred Klaffenboeck <fred_klaf@gmx.net>
2015-09-01 15:15:02 +0200

    deleted old (and stupid) safety copies

--- app/models/powder_data/quality_metric copy.rb
+++
@@ -1,7 +0,0 @@
-class PowderData::QualityMetric < ActiveRecord::Base
-  belongs_to :math_model_run
-  
-  def self.descendants
-    ObjectSpace.each_object(Class).select { |klass| klass < self }
-  end
-end
